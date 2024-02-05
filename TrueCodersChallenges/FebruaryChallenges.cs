196861462b6b158bdc89fbcd4c68dc491c3b9c59
Debra Winter <deb@debwinter.com>
2024-02-05 13:47:45 -0500

    Factorial for challenge 1

---
+++ TrueCodersChallenges/FebruaryChallenges.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace TrueCodersChallenges
+{
+	public class FebruaryChallenge
+	{
+		public FebruaryChallenge()
+		{
+		}
+
+		public int Factorial(int n)
+        {
+            int fact = 1;
+            for (int x = n; x > 0; x--)
+            {
+                fact *= x;
+            }
+            return fact;
+        }
+
+        public void ReportFactorial(int n)
+        {
+            Console.WriteLine($"The factorial of {n}");
+
+            Console.Write("is ");
+            for (int i = n; i > 1; i--)
+            {
+                Console.Write(i + " x ");
+            }
+            Console.WriteLine("1,");
+
+            Console.WriteLine($"which is equal to {Factorial(n)}.");
+        }
+    }
+}
+
---
+++ TrueCodersChallenges/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TrueCodersChallenges;
+class Program
+{
+    static void Main(string[] args)
+    {
+        FebruaryChallenge challenge1 = new FebruaryChallenge();
+
+        challenge1.ReportFactorial(5);
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        challenge1.ReportFactorial(10);
+    }
+}
+
